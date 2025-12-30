67aea37bef2a61c8520fe04eae0241a85f800e0c
bapel <bapel@null.net>
2025-12-31 00:02:10 +0700

    tes

--- app/services/OllamaService.php
+++ app/services/OllamaService.php
@@ -366,11 +366,6 @@ class OllamaService
                         if ($onToken && is_callable($onToken)) {
                             $onToken($token, $chunk['done'] ?? false);
                         }
-
-                        // RESTORED: Critical for Vercel Streaming
-                        if (ob_get_length())
-                            ob_flush();
-                        flush();
                     }
                 }
             }
